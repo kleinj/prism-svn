5fc3de60288a4d4e59f9d7ed82e4bed64d62f5c8
Joachim Klein <klein@tcs.inf.tu-dresden.de>
2017-08-15 23:46:59 +0200

    symbolic MDP checking: fairness for Rmax[ F ... ] not supported
    Check and throw exception if fairness is requested for Rmax[F].
    
    Rmin[F] and Rmax[C] computations are unaffected by PRISM's fairness.

--- prism/src/prism/NondetModelChecker.java
+++ prism/src/prism/NondetModelChecker.java
@@ -1423,6 +1423,12 @@ public class NondetModelChecker extends NonProbModelChecker
 		JDDNode b;
 		StateValues rewards = null;
 
+		if (fairness && !min) {
+			// Rmin computation is unaffected by fairness
+			JDD.Deref(statesOfInterest);
+			throw new PrismNotSupportedException("Maximum reward computation currently not supported under fairness.");
+		}
+
 		// currently, ignore statesOfInterest
 		JDD.Deref(statesOfInterest);
 
@@ -1466,6 +1472,12 @@ public class NondetModelChecker extends NonProbModelChecker
 		LTLProduct<NondetModel> modelProduct;
 		NondetModelChecker mcProduct;
 		long l;
+
+		if (fairness && !min) {
+			// Rmin computation is unaffected by fairness
+			JDD.Deref(statesOfInterest);
+			throw new PrismNotSupportedException("Maximum reward computation currently not supported under fairness.");
+		}
 
 		if (Expression.containsTemporalTimeBounds(expr)) {
 			if (model.getModelType().continuousTime()) {
@@ -2184,6 +2196,8 @@ public class NondetModelChecker extends NonProbModelChecker
 		StateValues rewards = null;
 		// Local copy of setting
 		int engine = this.engine;
+
+		// fairness does not matter for Rmax[ C ] computation
 
 		if (doIntervalIteration) {
 			throw new PrismNotSupportedException("Interval iteration for total rewards is currently not supported");
